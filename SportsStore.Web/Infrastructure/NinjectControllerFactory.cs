5ce89368dce613424dd8ac9e98ae0c814e4adb1f
fraser addison <fraseraddison@gmail.com>
2014-11-20 13:00:45 -0800

    added references to ninject and moq. added ninject controller factory to web infrastructure

---
+++ SportsStore.Web/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,41 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="NinjectControllerFactory.cs" company="Homewood Human Solutions">
+// // This file is subject to the terms and conditions defined in file 'LICENSE.txt', 
+// // which is part of this source code package.
+// // </copyright>
+// // <author>Fraser Addison</author>
+// // <created>20-11-2014</created>
+// //
+// // <summary>
+// // The NinjectControllerFactory.cs
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace SportsStore.Web.Infrastructure
+{
+    using System;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using Ninject;
+
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            // add bindings here
+        }
+    }
+}
